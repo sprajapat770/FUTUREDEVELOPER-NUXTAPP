850850fefcc76cc3a4d7bdacf5ec88747bcc980a
Suraj Prajapt <sprajapat770@gmail.com>
2021-08-27 09:30:36 +0530

    website 2nd push

---
+++ _nuxt/static/1630036086/About/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffuturedeveloper\u002F_nuxt\u002Fstatic\u002F1630036086",layout:"default",error:a,serverRendered:true,routePath:"\u002FAbout",config:{_app:{basePath:"\u002Ffuturedeveloper\u002F",assetsPath:"\u002Ffuturedeveloper\u002F_nuxt\u002F",cdnURL:a}}}}(null));
