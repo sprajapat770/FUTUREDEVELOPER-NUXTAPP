95740631613fd5cb77817211e8a5953a4dc5a535
Suraj Prajapt <sprajapat770@gmail.com>
2021-08-27 09:25:15 +0530

    website

---
+++ pages/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about-us">
+    <h1>About Us</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <h1>
+      Home Page
+    </h1>
+    <div class="links">
+      <NuxtLink to="/about">About</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
